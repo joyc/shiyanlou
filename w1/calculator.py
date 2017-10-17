fcc0808252da74d5f3307a49587b510784c88b20
joyc <joyc@live.jp>
2017-10-18 00:12:30 +0900

    task 1

--- w1/calculator.py
+++ w1/calculator.py
@@ -1,40 +1,51 @@
 #! /usr/bin/env python3
-"""calculate the person tax"""
+"""calculating the person tax"""
 
 import sys
 
-
+# define
 deductions = {0.03: 0, 0.1: 105, 0.2: 555, 0.25: 1005, 0.3: 2755, 0.35: 5505, 0.45: 13505}
 
-# if sys.argv[1] is not None:
-if len(sys.argv[1]) > 1:
-    try:
-        income = int(sys.argv[1])
-    except ValueError:
-        print("Parameter Error")
 
 def tax_rate(income):
-    "get tax rate with income"
+    """get tax rate with incom"""
     if income <= 1500:
-        tax_rate = 0.03
+        rate = 0.03
     elif income <= 4500:
-        tax_rate = 0.1
+        rate = 0.1
     elif income <= 9000:
-        tax_rate = 0.2
+        rate = 0.2
     elif income <= 35000:
-        tax_rate = 0.25
+        rate = 0.25
     elif income <= 55000:
-        tax_rate = 0.3
+        rate = 0.3
     elif income <= 80000:
-        tax_rate = 0.35
+        rate = 0.35
     else:
-        tax_rate = 0.45
-    return
+        rate = 0.45
+    return rate
 
-# def tax_payable(income):
-#     "calculate payable tax"
-    tax_rate(income)
-    deduction = deductions[tax_rate]
-    taxable_income = (income - 0 - 3500)
-    tax = taxable_income * tax_rate - deduction
-    print(income + '\'s tax is {:.2f}.'.format(tax))
+
+def tax_payable(income):
+    """calculate payable tax"""
+    if income > 3500:
+        taxable_income = (income - 0 - 3500)
+        taxrate = tax_rate(taxable_income)
+        deduction = deductions[taxrate]
+        tax = taxable_income * taxrate - deduction
+        print(str(income) + '\'s tax is {:.2f}.'.format(tax))
+    else:
+        tax = 0
+        print(str(income) + '\'s tax is {:.2f}.'.format(tax))
+
+
+if __name__ == '__main__':
+    # if sys.argv[1] is not None:
+    if len(sys.argv[1]) > 1:
+        try:
+            income = int(sys.argv[1])
+        except ValueError:
+            print("Parameter Error")
+        tax_payable(income)
+    else:
+        print("Parameter Error")
