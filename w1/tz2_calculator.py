90b5fea2c0e1116d6c0b464a2508e23792b881fc
joyc <joyc@live.jp>
2017-10-18 14:12:20 +0900

    update

--- w1/tz2_calculator.py
+++ w1/tz2_calculator.py
@@ -7,6 +7,11 @@ import sys
 deductions = {0.03: 0, 0.1: 105, 0.2: 555, 0.25: 1005, 0.3: 2755, 0.35: 5505, 0.45: 13505}
 
 
+def baoxian(income):
+    """bao xian"""
+    return income * (0.08 + 0.02 + 0.005 + 0.06)
+
+
 def tax_rate(income):
     """get tax rate with incom"""
     if income <= 1500:
@@ -26,17 +31,19 @@ def tax_rate(income):
     return rate
 
 
-def tax_payable(income):
+def after_tax(income):
     """calculate payable tax"""
+    bx = baoxian(income)
     if income > 3500:
-        taxable_income = (income - 0 - 3500)
+        taxable_income = (income - bx - 3500)
         taxrate = tax_rate(taxable_income)
         deduction = deductions[taxrate]
         tax = taxable_income * taxrate - deduction
-        print(str(income) + '\'s tax is {:.2f}.'.format(tax))
+        after = income - tax - bx
+        print(str(income) + '\'s after tax is {:.2f}.'.format(after))
     else:
-        tax = 0
-        print(str(income) + '\'s tax is {:.2f}.'.format(tax))
+        after = income - bx
+        print(str(income) + '\'s after tax is {:.2f}.'.format(after))
 
 
 if __name__ == '__main__':
@@ -46,6 +53,6 @@ if __name__ == '__main__':
             income = int(sys.argv[1])
         except ValueError:
             print("Parameter Error")
-        tax_payable(income)
+        after_tax(income)
     else:
         print("Parameter Error")
