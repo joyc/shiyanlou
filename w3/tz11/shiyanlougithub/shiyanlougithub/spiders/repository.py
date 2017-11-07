5ec0dcb57528e488e1028672ecc6bc572058ef91
joyc <joyc@live.jp>
2017-11-07 23:58:08 +0900

    tz11 init

---
+++ w3/tz11/shiyanlougithub/shiyanlougithub/spiders/repository.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from shiyanlougithub.items import ShiyanlougithubItem
+
+class RepositorySpider(scrapy.Spider):
+    name = 'shiyanlou-github'
+    allowed_domains = ['github.com']
+
+    @property
+    def start_urls(self):
+        url_tmpl = 'https://github.com/shiyanlou?page={}&tab=repositories'
+        return (url_tmpl.format(i) for i in range(1, 5))
+
+    def parse(self, response):
+        for repository in response.css('li.public'):
+            item = ShiyanlougithubItem ({
+                'name': repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first("\n\s*(.*)"),
+                'update_time': repository.xpath('.//relative-time/@datetime').extract_first()
+            })
+            yield item
