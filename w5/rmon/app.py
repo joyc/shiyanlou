c04abad5163f12a2f107f93ce33c355e1e8817c1
chuang.li <chuang.li.eb@bp.renesas.com>
2017-11-20 15:45:53 +0900

    w5 init

---
+++ w5/rmon/app.py
@@ -0,0 +1,17 @@
+""" app.py
+
+应用入口文件
+"""
+import urllib
+from rmon.app import create_app
+from rmon.models import db
+
+app = create_app()
+
+@app.cli.command()
+def init_db():
+    """初始化数据库
+    """
+    print("sqlite3 database file is %s" % app.config['SQLALCHEMY_DATABASE_URI'])
+    db.create_all()
+    
