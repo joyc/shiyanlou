e344a4ac4e650acbd0d1b5608ed7d50a2c1b4859
joyc <joyc@live.jp>
2018-05-08 23:57:24 +0900

    add new file

--- w3/lesson/shiyanlou/shiyanlou/login_spider.py
+++ w3/lesson/shiyanlou/shiyanlou/login_spider.py
@@ -3,17 +3,18 @@ import scrapy
 
 
 class LoginSpiderSpider(scrapy.Spider):
-    name = 'login_spider'
 
+    name = 'login_spider'
     start_urls = ['https://www.shiyanlou.com/login']
 
     """
     def parse(self, response):
         """
-        模拟登录的核心就在这里，scrapy 会下载 start_urls 里的
-        登录页面，将 response 传到这里，然后调用 FormRequest
-        模拟构造一个 POST 登录请求。FormRequest 继承自 Request，
-        所以 Request 的参数对它适用。FormRequest 有一类方法 from_response 用于快速构建 FormRequest 对象。from_response 方法会从第一步返回的 response 中获取请求的 url，form 表单信息等等，因此只需要指定必要的表单数据和回调函数即可。
+        模拟登录的核心就在这里,scrapy会下载 start_urls 里的登录页面,将 response 传到这里,
+        然后调用 FormRequest 模拟构造一个 POST 登录请求。FormRequest 继承自 Request，
+        所以 Request 的参数对它适用。FormRequest 有一类方法 from_response 用于快速构建 FormRequest 对象。
+        from_response 方法会从第一步返回的 response 中获取请求的 url，form 表单信息等等，
+        因此只需要指定必要的表单数据和回调函数即可。
         """
         return scrapy.FormRequest.from_response(
             # 第一个参数必须传入上一步返回的 response
@@ -33,6 +34,7 @@ class LoginSpiderSpider(scrapy.Spider):
     def parse_after_login(self, response):
         pass
     """
+
     def parse(self, response):
         # 获取表单的 csrf_token
         csrf_token = response.xpath('').extract_first()
